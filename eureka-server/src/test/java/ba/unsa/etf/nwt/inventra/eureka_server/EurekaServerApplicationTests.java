77851a700769b4b3ea345bc743286f5593580a4f
lheleg <lheleg1@etf.unsa.ba>
2025-04-07 01:30:52 +0200

    add Eureka server and configure health check

---
+++ eureka-server/src/test/java/ba/unsa/etf/nwt/inventra/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.inventra.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
